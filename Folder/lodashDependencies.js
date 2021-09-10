333a020e667a2d74b399e6355daaf214e9188403
Musaib Bilal <hotmusaib@gmail.com>
2021-09-10 08:57:58 +0530

    FlattenDeepMethod

---
+++ Folder/lodashDependencies.js
@@ -0,0 +1,7 @@
+//In the program we use lodash module
+const _ = require('lodash');
+
+const items = [1, [2, [3, [4]]]];
+//flattenDeep is a method of lodash
+const newItems = _.flattenDeep(items);
+console.log(newItems);
