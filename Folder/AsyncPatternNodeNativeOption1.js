f655f7963c798c2926e69209b2b69b9854461948
Musaib Bilal <hotmusaib@gmail.com>
2021-09-16 11:21:05 +0530

    Update

--- Folder/AsyncPatternNodeNativeOption1.js
+++ Folder/AsyncPatternNodeNativeOption1.js
@@ -8,7 +8,10 @@ const start = async () => {
         const first = await readFilePromise('first.txt', 'utf8')
         await writeFilePromise(
             'result.txt',
-            `This will be my text: ${first}`
+            `This will be my text: ${first}`,
+            //If there is no file with name "result.txt". It will be created automatically.
+            //If the file is already present in the folder, use append flag.
+            { flag: 'a' }
         )
         console.log(first)
     } catch {
