9ad7565e034ce7f5404d78b02f958828082949d1
Musaib Bilal <hotmusaib@gmail.com>
2021-09-19 09:22:13 +0530

    Passing multiple arguments

---
+++ Folder/eventarguments.js
@@ -0,0 +1,14 @@
+const EventEmitter = require('events')
+
+const customEmitter = new EventEmitter()
+
+customEmitter.on('response', (name, id) => {
+    console.log(`data received user ${name} with id:${id}`);
+})
+
+customEmitter.on('response', () => {
+    console.log(`data received!`);
+})
+
+//We can send multiple arguments in the emit method
+customEmitter.emit('response', 'Musaib', 25)
